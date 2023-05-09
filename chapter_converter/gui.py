afec54f9d87e6b836e5937e4a97c2a6be833adb0
fireattack <human.peng@gmail.com>
2023-05-09 20:24:41 +0800

    Add info about gui

--- chapter_converter/gui.py
+++ chapter_converter/gui.py
@@ -1,4 +1,9 @@
-from gooey import Gooey
+try:
+    from gooey import Gooey
+except ImportError:
+    print("Please install `gooey` manually to use the GUI:")
+    print("`pip install attrdict3`, then `pip install gooey` (in two transactions)")
+    exit(1)
 
 from .chapter_converter import main
 
